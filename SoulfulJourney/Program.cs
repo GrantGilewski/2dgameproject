b57a5a4c498c03a704d0630e7f2e972cf6a0e06f
Questionxble <christian.henken02@gmail.com>
2025-09-22 19:59:49 -0500

    commit

---
+++ SoulfulJourney/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SoulfulJourney
+{
+    public class GameEngine : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch? _spriteBatch;
+
+        public GameEngine()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 600;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(new Color(255, 255, 255)); // White
+
+            // Drawing code goes here
+
+            base.Draw(gameTime);
+        }
+
+        static void Main(string[] args)
+        {
+            using (var game = new GameEngine())
+                game.Run();
+        }
+    }
+}
